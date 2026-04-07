a55767ebfcd8f53c2c4f02cba8197139117aefdb
Arup Halder <aruphalder2526@gmail.com>
2026-04-07 00:59:37 -0300

    expo-go compatible

---
+++ src/config/keys.js
@@ -0,0 +1,11 @@
+export const KEYS = {
+  GOOGLE_MAPS_API_KEY: 'AIzaSyCB7KfUZhRpmCDVchecWo-znUhqCB6qjIk',
+  FIREBASE: {
+    apiKey: 'AIzaSyDrZZHzp8u4jdZn5k5xoJCvQqC9BXw7JlE',
+    authDomain: 'dalparking-19789.firebaseapp.com',
+    projectId: 'dalparking-19789',
+    storageBucket: 'dalparking-19789.firebasestorage.app',
+    messagingSenderId: '267186162003',
+    appId: '1:267186162003:web:1a5522a2e5e351104739a6',
+  },
+};
